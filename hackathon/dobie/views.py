b6af34f30eb9c2c66f64ee78c04b6666899c1f2e
Yotam Vaknin <tomirendo@gmail.com>
2018-04-26 18:12:09 +0300

    added facebook login django

--- hackathon/dobie/views.py
+++ hackathon/dobie/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from dobie.models import Users, Responses, Orders
 import json
+from facebook_api import facebook_api
+from datetime import datetime
+
 # Create your views here.
 
 def facebookLogin(request):
-    return HttpResponse(str(request.__dict__))
+    access_token = request.GET['access_token']
+    user_info = facebook_api.get_user_info(access_token)
+    query = Users.objects.filter(facebook_id= user_info.facebook_id)
+    if query:
+        user = query[0]
+        user.first_name = user_info.first_name
+        user.last_name = user_info.last_name
+        user.access_token = access_token
+        user.last_change = datetime.now()
+    else : 
+        user = Users(first_name = user_info.first_name,
+           last_name = user_info.last_name,
+           name = user_info.full_name,
+           access_token = access_token,
+           facebook_id = user_info.facebook_id,
+           create_date = datetime.now(),
+           last_change = datetime.now())
+    user.save()
+    return HttpResponse(json.dumps({'error' : False}))
 
 def getOrders(request):
     dict = {}
