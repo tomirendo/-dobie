237690c99dd2a181128bdf4cf01d888b3ab29cc2
Itamar Salhov <itamar.salhov@gmail.com>
2018-04-26 12:06:09 +0300

    blah

--- dobie/src/index.js
+++ dobie/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component{
-    render () {
-        return <div> Hello </div>;
-    }
-}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends React.Component{
+	render(){
+		return <div> Hello </div>;
+	}
+}
+ReactDOM.render(<App />, 
+	document.getElementById('root'));
+
