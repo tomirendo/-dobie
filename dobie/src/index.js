7e7c42ec63c92c92e8b73240f633839471065512
Itamar Salhov <itamar.salhov@gmail.com>
2018-04-26 13:00:30 +0300

    added design

--- dobie/src/index.js
+++ dobie/src/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Button , ButtonToolbar } from 'react-bootstrap';
+class Boots extends React.Component {
+  render() {
+    return (
+      <div className="Boots">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
+  
+
+        </div>
+    );
+  }
+}
 
 
-class App extends React.Component{
+class Hero extends React.Component {
 	render(){
-		return <div> Hello </div>;
+		return(
+			<ButtonToolbar>
+				<Button bsStyle = "success"> press me </Button>
+			</ButtonToolbar>
+		)
+	}
+
+}
+
+
+class FullSite extends React.Component{
+	render(){
+		return (
+			<div> 
+				<Boots />
+				<Hero />
+
+
+			</div>
+		)
+		
 	}
 }
-ReactDOM.render(<App />, 
+
+
+
+
+
+ReactDOM.render(<FullSite />, 
 	document.getElementById('root'));
 
